d9bea46e120fef03aad85f152e9b842a6b97689e
MJServices <mjservices410@gmail.com>
2026-01-31 22:31:15 +0500

    work done hogaye

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -66,21 +66,7 @@ const Header = ({ toggleSidebar }: HeaderProps) => {
             </div>
 
             <div className="flex items-center gap-3 lg:gap-6 ml-4">
-                <button className="relative text-gray-500 hover:text-gray-700">
-                    <Bell className="w-5 h-5" />
-                    <span className="absolute -top-1 -right-1 w-4 h-4 bg-red-500 text-white text-[10px] flex items-center justify-center rounded-full">
-                        3
-                    </span>
-                </button>
-
-                <div className="flex items-center gap-2 cursor-pointer">
-                    <div className="hidden lg:block text-right">
-                        <span className="block text-sm font-medium text-gray-700">Admin User</span>
-                    </div>
-                    {/* Mobile optimization: just the chevron or avatar if existed */}
-                    <span className="lg:hidden text-sm font-medium text-gray-700">Admin</span>
-                    <ChevronDown className="w-4 h-4 text-gray-500" />
-                </div>
+                {/* Profile/Notification section removed as per request */}
             </div>
         </header>
     );
